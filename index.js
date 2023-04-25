7d1d2080b5d478de8fec0035a7e2a6cb0fa12378
shubham <pghotkar931@gmail.com>
2023-04-25 08:53:38 +0530

    completed ; all work

--- index.js
+++ index.js
@@ -6,18 +6,25 @@ const addWater = document.querySelector(".empty-water");
 const remVolumn = document.querySelector(".span-volumn");
 const addVolumn = document.querySelector(".fill-span");
 
+// Array
+let set = new Set();
+
 // VARIABLES
 let toggle = false;
 let remWaterHeight;
 let waterRemain;
 let WaterAdded;
+let waterInd;
 
 // FUCTIONS
 function addBackEffect(ind) {
-  for (let i = 0; i <= ind; i++) {
+  // for add the class
+  for (let i = 0; toggle ? i < ind : i <= ind; i++) {
     jars[i].classList.add("fill");
   }
-  for (let i = ind + 1; i < jars.length; i++) {
+
+  // for remove the fill class on remaining element
+  for (let i = toggle ? ind : ind + 1; i < jars.length; i++) {
     jars[i].classList.remove("fill");
   }
 }
@@ -25,20 +32,28 @@ function addBackEffect(ind) {
 // LOGIC
 jars.forEach((jar, ind) => {
   jar.addEventListener("click", function () {
-    //
+    // call to the function
     addBackEffect(ind);
 
-    remWaterHeight = 12.8 * (ind + 1);
+    // set index of water level base on the toggle
+    waterInd = toggle ? ind : ind + 1;
+    remWaterHeight = 12.5 * waterInd;
 
-    //
+    //give heigth and class to the big jar
     removeWater.style.height = `${100 - remWaterHeight}%`;
     addWater.style.height = `${remWaterHeight}%`;
     addWater.classList.add("fill");
 
     // SET VALUE
-    waterRemain = (200 - 25 * (ind + 1)) * 0.01;
-    WaterAdded = 12.5 * (ind + 1);
+
+    waterRemain = (200 - 25 * waterInd) * 0.01;
+    WaterAdded = 12.5 * waterInd;
+
+    // change the text as per water
     remVolumn.textContent = `${waterRemain}L`;
     addVolumn.textContent = `${WaterAdded}%`;
+
+    // toggle
+    toggle = !toggle;
   });
 });
